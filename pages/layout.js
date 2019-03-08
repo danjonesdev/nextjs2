9cac349048d594ae539cce75a371200c3883066f
Danny Jones <dannyjones360@gmail.com>
2019-03-08 12:25:53 +0000

    Revs

--- pages/layout.js
+++ pages/layout.js
@@ -1,29 +1,13 @@
 import Head from "next/head";
-import { Link } from "../routes";
-
 import config from "../config";
 
+import HeaderNav from "../components/header-nav";
+import Footer from "../components/footer";
+
 export default class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuOpen: false
-    };
-    this.handleMenuOpen = this.handleMenuOpen.bind(this);
-    this.handleClickMenuItem = this.handleClickMenuItem.bind(this);
-  }
-
-  handleMenuOpen() {
-    this.setState({ menuOpen: !this.state.menuOpen });
-  }
-
-  handleClickMenuItem() {
-    this.setState({ menuOpen: false });
-  }
-
   renderHead() {
     return (
-      <Head className="{this.props.className}">
+      <Head>
         <title>
           {this.props.title || "Not Found"} - {this.props.layout.data.site_name}
         </title>
@@ -38,18 +22,6 @@ export default class extends React.Component {
         />
         <meta name="theme-color" content={config.meta.theme_colour} />
         <meta name="author" content="Dan" />
-
-        <link href="/static/images/punch.png" rel="icon" type="image/png" />
-        <link
-          href="https://fonts.googleapis.com/css?family=PT+Mono"
-          rel="stylesheet"
-        />
-        <link
-          href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
-          rel="stylesheet"
-          integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
-          crossOrigin="anonymous"
-        />
         <link href="/_next/static/style.css" rel="stylesheet" />
       </Head>
     );
@@ -58,21 +30,9 @@ export default class extends React.Component {
     return (
       <React.Fragment>
         {this.renderHead()}
-        <header className="header">
-          <nav className="nav">
-            <Link to="/">
-              <img
-                className="w3"
-                src="/static/images/punch.png"
-                alt={this.props.layout.data.site_name}
-              />
-            </Link>
-          </nav>
-        </header>
-
+        <HeaderNav {...this.props} />
         <main>{this.props.children}</main>
-
-        <footer className="footer">footer</footer>
+        <Footer {...this.props} />
       </React.Fragment>
     );
   }
