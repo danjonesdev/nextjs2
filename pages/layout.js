f3e52ca68f9eb4953c8c3ca026b722b3703a29c3
dan jones <dannyjones360@gmail.com>
2019-03-11 20:20:50 +0000

    Add extra meta

--- pages/layout.js
+++ pages/layout.js
@@ -32,6 +32,10 @@ export default class extends React.Component {
 
     return (
       <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        
         <title>
           {titleVal}
           {' - '}
