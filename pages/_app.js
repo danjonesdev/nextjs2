6f837a7929dc73e80e95a83d4a3b44737c928446
Danny Jones <dannyjones360@gmail.com>
2019-03-14 14:24:45 +0000

    Add NProgress

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,13 @@
 import App, { Container } from 'next/app';
 import React from 'react';
 import { PageTransition } from 'next-page-transitions';
+import NProgress from 'next-nprogress/component';
 
 import { Client } from '../components/prismic';
 import '../assets/stylesheets/main.scss';
 
 export default class extends App {
-  static async getInitialProps({
-    Component, ctx, req,
-  }) {
+  static async getInitialProps({ Component, ctx, req }) {
     let pageProps = {};
 
     if (Component.getInitialProps) {
@@ -23,6 +22,7 @@ export default class extends App {
     const { Component, pageProps, layout } = this.props;
     return (
       <Container>
+        <NProgress color="#000" />
         <PageTransition timeout={300} classNames="page-transition">
           <Component {...Object.assign(pageProps, { layout })} />
         </PageTransition>
